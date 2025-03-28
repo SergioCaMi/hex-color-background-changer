db51feb2bdb5d609b872e7b7c344e2c35d261229
SergioCaMi <sergiocami84@gmail.com>
2025-03-28 09:32:01 +0100

    pequeño arreglo deconversión en el default del switch

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-// Implementar solución aquí
-// Crearemos el número hexadecimal
-
 const miBoton = document.querySelector(".btn");
 
 miBoton.addEventListener("click", (event) => {
@@ -9,27 +6,28 @@ miBoton.addEventListener("click", (event) => {
   for (let x = 0; x < 6; x++) {
     numero = Math.floor(Math.random() * 16);
     switch (numero) {
-      case 9:
+      case 10:
         colorHex.push("A");
         break;
-      case 10:
+      case 11:
         colorHex.push("B");
         break;
-      case 11:
+      case 12:
         colorHex.push("C");
         break;
-      case 12:
+      case 13:
         colorHex.push("D");
         break;
-      case 13:
+      case 14:
         colorHex.push("E");
         break;
-      case 14:
+      case 15:
         colorHex.push("F");
         break;
       default:
-        colorHex.push(numero);
+        colorHex.push(numero.toString()); 
     }
+    console.log(colorHex);
   }
   document.querySelector("#hex-value").textContent = "#" + colorHex.join("");
   document.querySelector("body").style.backgroundColor = "#" + colorHex.join("");
